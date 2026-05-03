a3b3eca407a1d59290f649118f8629561fe735c4
IT24100586 <vidunimasenadeera@gmail.com>
2026-05-03 12:44:17 +0530

    Add IT24102809 implementation

--- Backend/src/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAnalytics } = require('../controllers/analyticsController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/')
-  .get(protect, getAnalytics);
-
-module.exports = router;
